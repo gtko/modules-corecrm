b6387242ef31c1823a36214720dfb357ad010b9d
gtko <gtux.prog@gmail.com>
2021-09-14 12:29:26 +0200

    WIP change datalist

--- Contracts/Repositories/FlowRepositoryContract.php
+++ Contracts/Repositories/FlowRepositoryContract.php
@@ -4,12 +4,11 @@
 namespace Modules\CoreCRM\Contracts\Repositories;
 
 
-use Modules\BaseCore\Contracts\Repositories\RelationsRepositoryContract;
 use Modules\CoreCRM\Flow\Interfaces\FlowAttributes;
 use Modules\CoreCRM\Interfaces\Flowable;
 use Modules\CoreCRM\Models\Flow;
 
-interface FlowRepositoryContract extends RelationsRepositoryContract
+interface FlowRepositoryContract
 {
     public function createFlow(Flowable $flowable, FlowAttributes $flowAttributes): Flow;
 
