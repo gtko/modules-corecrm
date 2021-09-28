96f219eb6b945b83cbd6fcf6fb0ba1deee081b2d
gtko <gtux.prog@gmail.com>
2021-09-28 15:47:51 +0200

    WIP

--- Resources/views/app/dossiers/show.blade.php
+++ Resources/views/app/dossiers/show.blade.php
@@ -60,15 +60,24 @@
                         <x-basecore::icon-label icon="devis" label="Devis" :size="20"/>
                     </x-basecore::nav.menu-item>
 
-{{--                    <x-basecore::nav.menu-item name="call">--}}
-{{--                        <x-basecore::icon-label icon="folder" label="Appels"/>--}}
-{{--                    </x-basecore::nav.menu-item>--}}
-{{--                    <x-basecore::nav.menu-item name="email">--}}
-{{--                        <x-basecore::icon-label icon="folder" label="Emails"/>--}}
-{{--                    </x-basecore::nav.menu-item>--}}
-{{--                    <x-basecore::nav.menu-item name="documents">--}}
-{{--                        <x-basecore::icon-label icon="document" label="Documents"/>--}}
-{{--                    </x-basecore::nav.menu-item>--}}
+                    @if(in_array('call', config('corecrm.features')))
+                    <x-basecore::nav.menu-item name="call">
+                        <x-basecore::icon-label icon="folder" label="Appels"/>
+                    </x-basecore::nav.menu-item>
+                    @endif
+
+                    @if(in_array('email', config('corecrm.features')))
+                    <x-basecore::nav.menu-item name="email">
+                        <x-basecore::icon-label icon="folder" label="Emails"/>
+                    </x-basecore::nav.menu-item>
+                    @endif
+
+                    @if(in_array('document', config('corecrm.features')))
+                    <x-basecore::nav.menu-item name="documents">
+                        <x-basecore::icon-label icon="document" label="Documents"/>
+                    </x-basecore::nav.menu-item>
+                    @endif
+
                     <x-basecore::resolve-type-view
                         :contrat-view-class="\Modules\CoreCRM\Contracts\Views\Dossiers\DossierTabLabelViewContract::class"
                         :arguments="['client' => $client, 'dossier' => $dossier]"
@@ -151,19 +160,21 @@
                     :contrat-view-class="\Modules\CoreCRM\Contracts\Views\Dossiers\DossierTabViewContract::class"
                     :arguments="['client' => $client, 'dossier' => $dossier]"
                 />
-
-{{--                <x-basecore::nav.tab name="call">--}}
-{{--                    <livewire:appelcrm::appel :dossier-id="$dossier->id" :client-id="$client->id"/>--}}
-{{--                </x-basecore::nav.tab>--}}
-
-{{--                <x-basecore::nav.tab name="email">--}}
-{{--                    email--}}
-{{--                </x-basecore::nav.tab>--}}
-
-{{--                <x-basecore::nav.tab name="documents">--}}
-{{--                    <livewire:corecrm::document :dossier-id="$dossier->id" :client-id="$client->id"/>--}}
-{{--                </x-basecore::nav.tab>--}}
-
+                @if(in_array('call', config('corecrm.features')))
+                    <x-basecore::nav.tab name="call">
+                        <livewire:appelcrm::appel :dossier-id="$dossier->id" :client-id="$client->id"/>
+                    </x-basecore::nav.tab>
+                @endif
+                @if(in_array('email', config('corecrm.features')))
+                <x-basecore::nav.tab name="email">
+                    email
+                </x-basecore::nav.tab>
+                @endif
+                @if(in_array('document', config('corecrm.features')))
+                <x-basecore::nav.tab name="documents">
+                    <livewire:corecrm::document :dossier-id="$dossier->id" :client-id="$client->id"/>
+                </x-basecore::nav.tab>
+                @endif
 
 
             </x-basecore::partials.card>
