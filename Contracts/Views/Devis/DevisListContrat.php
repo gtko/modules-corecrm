7557744a73279c93d2da955dbdf5ecd57baa4697
gtko <gtux.prog@gmail.com>
2021-10-10 14:58:49 +0200

    Add devislist

--- Contracts/Views/Devis/DevisListContrat.php
+++ Contracts/Views/Devis/DevisListContrat.php
@@ -5,5 +5,5 @@ namespace Modules\CoreCRM\Contracts\Views\Devis;
 
 use Modules\BaseCore\Contracts\Views\ViewContract;
 
-interface DevisSidebarStatusViewContract extends ViewContract
+interface DevisListContrat extends ViewContract
 {}
--- Resources/views/app/dossiers/show.blade.php
+++ Resources/views/app/dossiers/show.blade.php
@@ -93,71 +93,63 @@
                 </x-basecore::nav.tab>
 
                 <x-basecore::nav.tab name="devis">
-                    <div class="overflow-x-auto">
-                        <table class="table mt-5">
-                            <thead>
-                            <tr class="text-gray-700">
-                                <th class="whitespace-nowrap" colspan="3">
-                                    {{$devis->count()}} devis
-                                </th>
-{{--                             @foreach($brands as $brand)--}}
-{{--                                 <th colspan="2">{{$brand->label}}</th>--}}
-{{--                             @endforeach--}}
-                                <th></th>
-                            </tr>
-                            <tr class="bg-gray-100 text-gray-700">
-                                <th class="whitespace-nowrap">#</th>
-                                <th class="whitespace-nowrap">Commercial</th>
-                                <th class="whitespace-nowrap">Date du devis</th>
-{{--                              @foreach($brands as $brand)--}}
-{{--                                  <th class="whitespace-nowrap">Tarif</th>--}}
-{{--                                  <th class="whitespace-nowrap"></th>--}}
-{{--                              @endforeach--}}
-                                <th class="whitespace-nowrap">Actions</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($devis as $devi)
-                                <tr>
-                                    <td class="border-b dark:border-dark-5">
-                                        <a href="{{route('devis.edit', [$client, $dossier, $devi])}}">#{{$devi->ref}}</a>
-                                    </td>
-                                    <td class="border-b dark:border-dark-5">
-                                        {{$devi->commercial->format_name}}
-                                    </td>
-                                    <td class="border-b dark:border-dark-5">
-                                        {{$devi->created_at->format('d/m/Y H:i')}}
-                                    </td>
-                                    {{--    @foreach($brands as $brand)--}}
-                                    {{--        <td class="border-b dark:border-dark-5">{{$devi->data['brands'][$brand->id] ?? 0}}€</td>--}}
-                                    {{--        <td class="border-b dark:border-dark-5"></td>--}}
-                                    {{--    @endforeach--}}
-                                    <td class="border-b dark:border-dark-5">
-                                        <div class="flex">
-                                            @can('update', $devi)
-                                                <a class="flex items-center mr-3"
-                                                   href="{{route('devis.edit', [$client, $dossier, $devi])}}">
-                                                    @icon("edit", null,"w-4 h-4 mr-1")
-                                                </a>
-                                            @endcan
-
-                                            <x-corecrm::link-devis :devis="$devi" class="flex items-center mr-3">
-                                                @icon("show", null,"w-4 h-4 mr-1")
-                                            </x-corecrm::link-devis>
-
-                                            <a class='ignore-link' href="{{route('pdf-devis-download', $devi)}}">
-                                                @icon('pdf', null, 'w-4 h-4 mr-1')
-                                            </a>
-                                        </div>
-                                    </td>
+                    <x-basecore::resolve-type-view
+                        :contrat-view-class="\Modules\CoreCRM\Contracts\Views\Devis\DevisListContrat::class"
+                        :arguments="['client' => $client, 'dossier' => $dossier, 'devis' => $devis]"
+                    >
+                        <div class="overflow-x-auto">
+                            <table class="table mt-5">
+                                <thead>
+                                <tr class="text-gray-700">
+                                    <th class="whitespace-nowrap" colspan="3">
+                                        {{$devis->count()}} devis
+                                    </th>
+                                    <th></th>
                                 </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="mt-4">
-                        {{$devis->links()}}
-                    </div>
+                                <tr class="bg-gray-100 text-gray-700">
+                                    <th class="whitespace-nowrap">#</th>
+                                    <th class="whitespace-nowrap">Commercial</th>
+                                    <th class="whitespace-nowrap">Date du devis</th>
+                                    <th class="whitespace-nowrap">Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($devis as $devi)
+                                    <tr>
+                                        <td class="border-b dark:border-dark-5">
+                                            <a href="{{route('devis.edit', [$client, $dossier, $devi])}}">#{{$devi->ref}}</a>
+                                        </td>
+                                        <td class="border-b dark:border-dark-5">
+                                            {{$devi->commercial->format_name}}
+                                        </td>
+                                        <td class="border-b dark:border-dark-5">
+                                            {{$devi->created_at->format('d/m/Y H:i')}}
+                                        </td>
+                                        <td class="border-b dark:border-dark-5">
+                                            <div class="flex">
+                                                @can('update', $devi)
+                                                    <a class="flex items-center mr-3"
+                                                       href="{{route('devis.edit', [$client, $dossier, $devi])}}">
+                                                        @icon("edit", null,"w-4 h-4 mr-1")
+                                                    </a>
+                                                @endcan
+                                                <x-corecrm::link-devis :devis="$devi" class="flex items-center mr-3">
+                                                    @icon("show", null,"w-4 h-4 mr-1")
+                                                </x-corecrm::link-devis>
+                                                <a class='ignore-link' href="{{route('pdf-devis-download', $devi)}}">
+                                                    @icon('pdf', null, 'w-4 h-4 mr-1')
+                                                </a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="mt-4">
+                            {{$devis->links()}}
+                        </div>
+                    </x-basecore::resolve-type-view>
                 </x-basecore::nav.tab>
 
                 <x-basecore::resolve-type-view
