891266b56e6fd4915e49d8561cf62889635d845c
gtko <gtux.prog@gmail.com>
2021-11-07 15:13:43 +0100

    Revert "Revert "add commentaire du devis dans la timeline""
    This reverts commit 03262aefebcbf0bb6d42485a913d6d25907a1a75.

--- Resources/views/components/timeline/client/dossier/devis/update.blade.php
+++ Resources/views/components/timeline/client/dossier/devis/update.blade.php
@@ -5,13 +5,19 @@
     <div class="flex items-center">
         <div class="font-medium">
             Mise à jour du
-            <x-corecrm::timeline.timeline-item-link :url="route('devis.edit', [$flow->datas->getDevis()->dossier->client, $flow->datas->getDevis()->dossier, $flow->datas->getDevis()])">
+            <x-corecrm::timeline.timeline-item-link
+                :url="route('devis.edit', [$flow->datas->getDevis()->dossier->client, $flow->datas->getDevis()->dossier, $flow->datas->getDevis()])">
                 devis#{{$flow->datas->getDevis()->ref}}
             </x-corecrm::timeline.timeline-item-link>
             par
             <x-corecrm::timeline.timeline-item-link :url="route('users.show', $flow->datas->getUser())">
                 {{$flow->datas->getUser()->format_name}}
             </x-corecrm::timeline.timeline-item-link>
+            @if(array_key_exists('commentaire', $flow->datas->getData()) && $flow->datas->getData()['commentaire'] != '')
+                <div>
+                    Avec le commentaire : {{ $flow->datas->getData()['commentaire']}}
+                </div>
+            @endif
         </div>
         <div class="text-xs text-gray-500 ml-auto">{{$flow->created_at->format('H:i')}}</div>
     </div>
