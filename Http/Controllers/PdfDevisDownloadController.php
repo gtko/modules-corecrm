daef7f260d0dda7ee3fdc434737ab52ff16e3770
gtko <gtko@users.noreply.github.com>
2021-11-18 15:01:25 +0100

    WIP

--- Http/Controllers/PdfDevisDownloadController.php
+++ Http/Controllers/PdfDevisDownloadController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Str;
 use Modules\CoreCRM\Actions\Devis\GenerateLinkDevis;
 use Modules\CoreCRM\Contracts\Entities\DevisEntities;
 use Modules\BaseCore\Contracts\Services\PdfContract;
+use Modules\CrmBe\Librairies\Yousign\YousignApi;
 
 class PdfDevisDownloadController extends Controller
 {
@@ -18,4 +19,30 @@ class PdfDevisDownloadController extends Controller
 
         return $pdfService->downloadPdf($filename);
     }
+
+    public function signed(DevisEntities $devis){
+        /*
+         * Token
+         */
+        $token = '2be2e571e2edb78da7f3ffcb7d237972';
+        /*
+         * Production mode
+         */
+        $production = false;
+
+        /*
+         * Instanciate API wrapper
+         */
+        $file = $devis->yousign_data['member']['fileObjects'][0]['file']['id'] ?? '';
+        if($file) {
+            $yousign = new YousignApi($token, $production);
+            $download = $yousign->getDownload($file);
+            $filename = $devis->ref . '-' . Str::slug($devis->dossier->client->format_name) . '-' . $devis->created_at->format('d-m-Y') . '-signé' . '.pdf';
+            return response()->streamDownload(function () use ($download) {
+                echo base64_decode($download);
+            }, $filename);
+
+        }
+    }
+
 }
