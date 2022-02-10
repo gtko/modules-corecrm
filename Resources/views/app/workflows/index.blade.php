6e54073b3f226bc41530d3ee8283ee3120be111b
gtko <gtko@users.noreply.github.com>
2022-02-10 10:26:15 +0100

    WIP

--- Resources/views/app/workflows/index.blade.php
+++ Resources/views/app/workflows/index.blade.php
@@ -3,6 +3,13 @@
         <x-basecore::breadcrumb-item>Workflow</x-basecore::breadcrumb-item>
     </x-slot>
     <x-basecore::layout.panel-full>
+{{--        <div class="grid grid-cols-4">--}}
+{{--        @foreach($workflows as $workflow)--}}
+{{--            <div>--}}
+{{--                {{$workflow->name}}--}}
+{{--            </div>--}}
+{{--        @endforeach--}}
+{{--        </div>--}}
         <livewire:datalistcrm::data-list :title="'Workflow'" :type="Modules\CoreCRM\DataLists\WorkflowDataList::class"/>
     </x-basecore::layout.panel-full>
 </x-basecore::app-layout>
