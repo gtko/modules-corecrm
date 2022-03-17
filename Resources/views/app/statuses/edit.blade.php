56b75a687d1816085fa7c4a6d674b4e13662d1b5
gtko <gtko@users.noreply.github.com>
2022-03-17 10:56:19 +0100

    Fix status quand pas de pipeline

--- Resources/views/app/statuses/edit.blade.php
+++ Resources/views/app/statuses/edit.blade.php
@@ -1,7 +1,7 @@
 <x-basecore::app-layout>
     <x-slot name="breadcrumb">
         <x-basecore::breadcrumb-item :href="route('statuses.index')">Statuts</x-basecore::breadcrumb-item>
-        <x-basecore::breadcrumb-item :href="route('statuses.show', $status)">{{$status->label}}</x-basecore::breadcrumb-item>
+        <x-basecore::breadcrumb-item :href="route('statuses.edit', $status)">{{$status->label}}</x-basecore::breadcrumb-item>
         <x-basecore::breadcrumb-item>Edit</x-basecore::breadcrumb-item>
     </x-slot>
     <x-slot name="header">
@@ -10,8 +10,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <x-basecore::layout.panel-left>
             <x-basecore::partials.card>
                 <x-slot name="title">
                     <a href="{{ route('statuses.index') }}" class="mr-4"
@@ -26,7 +25,7 @@
                 >
                     @include('corecrm::app.statuses.form-inputs')
 
-                    <div class="mt-10">
+                    <div class="mt-10 flex justify-between items-center">
                         <a href="{{ route('statuses.index') }}" class="button">
                             <i
                                 class="
@@ -39,14 +38,9 @@
                             @lang('basecore::crud.common.back')
                         </a>
 
-                        <a href="{{ route('statuses.create') }}" class="button">
-                            <i class="mr-1 icon ion-md-add text-primary"></i>
-                            @lang('basecore::crud.common.create')
-                        </a>
-
                         <button
                             type="submit"
-                            class="button button-primary float-right"
+                            class="btn btn-primary"
                         >
                             <i class="mr-1 icon ion-md-save"></i>
                             @lang('basecore::crud.common.update')
@@ -54,6 +48,5 @@
                     </div>
                 </x-basecore::form>
             </x-basecore::partials.card>
-        </div>
-    </div>
+    </x-basecore::layout.panel-left>
 </x-basecore::app-layout>
