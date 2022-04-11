067db898f812e8d1a0f60552d826e187291b94b8
gtko <gtko@users.noreply.github.com>
2022-04-12 00:51:32 +0200

    WIP

--- Resources/views/livewire/dossier-tasks.blade.php
+++ Resources/views/livewire/dossier-tasks.blade.php
@@ -7,23 +7,27 @@
         </h2>
     </div>
 
-    <div>
-        <div class="intro-y inbox box mt-5">
+    <div wire:poll.visible>
+        <div class="intro-y inbox">
             <div class="p-5 flex flex-col-reverse sm:flex-row text-slate-500 border-b border-slate-200/60">
                 <div class="flex items-center mt-3 sm:mt-0 border-t sm:border-0 border-slate-200/60 pt-5 sm:pt-0 mt-5 sm:mt-0 -mx-5 sm:mx-0 px-5 sm:px-0">
-                    <span class="flex items-center justify-center">
-                        <svg class='h-4 w-4 mr-1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M481.2 33.81c-8.938-3.656-19.31-1.656-26.16 5.219l-50.51 50.51C364.3 53.81 312.1 32 256 32C157 32 68.53 98.31 40.91 193.3C37.19 206 44.5 219.3 57.22 223c12.81 3.781 26.06-3.625 29.75-16.31C108.7 132.1 178.2 80 256 80c43.12 0 83.35 16.42 114.7 43.4l-59.63 59.63c-6.875 6.875-8.906 17.19-5.219 26.16c3.719 8.969 12.47 14.81 22.19 14.81h143.9C485.2 223.1 496 213.3 496 200v-144C496 46.28 490.2 37.53 481.2 33.81zM454.7 288.1c-12.78-3.75-26.06 3.594-29.75 16.31C403.3 379.9 333.8 432 255.1 432c-43.12 0-83.38-16.42-114.7-43.41l59.62-59.62c6.875-6.875 8.891-17.03 5.203-26C202.4 294 193.7 288 183.1 288H40.05c-13.25 0-24.11 10.74-24.11 23.99v144c0 9.719 5.844 18.47 14.81 22.19C33.72 479.4 36.84 480 39.94 480c6.25 0 12.38-2.438 16.97-7.031l50.51-50.52C147.6 458.2 199.9 480 255.1 480c99 0 187.4-66.31 215.1-161.3C474.8 305.1 467.4 292.7 454.7 288.1z"/></svg>
-                        refresh
-                    </span>
                 </div>
                 <div class="flex items-center sm:ml-auto">
                     <div class="">{{$tasks->count()}} tâche{{($tasks->count() > 1)?'s':''}}</div>
                 </div>
             </div>
             <div class="overflow-x-auto sm:overflow-x-visible">
-                @foreach($tasks as $task)
-                        <livewire:corecrm::dossier-tasks-item :task="$task" :key="$task->id" />
-                @endforeach
+                @forelse($tasks as $task)
+                    <livewire:corecrm::dossier-tasks-item :task="$task" :key="$task->id" />
+                @empty
+                    <div class="text-center mt-10">
+                        <svg class="mx-auto h-12 w-12 text-gray-400" fill='currentColor' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M485.7 26.34c-3.125-3.125-8.188-3.125-11.31 0l-77.19 77.19C360.1 69.17 310.6 48 256 48C141.1 48 48 141.1 48 256c0 54.56 21.17 104.1 55.54 141.2l-77.19 77.19c-3.125 3.125-3.125 8.188 0 11.31C27.91 487.2 29.94 488 32 488s4.094-.7813 5.656-2.344l77.19-77.19C151.9 442.8 201.4 464 256 464c114.9 0 208-93.13 208-208c0-54.56-21.17-104.1-55.54-141.2l77.19-77.19C488.8 34.53 488.8 29.47 485.7 26.34zM64 256c0-105.9 86.13-192 192-192c50.06 0 95.56 19.42 129.8 50.91L114.9 385.8C83.42 351.6 64 306.1 64 256zM448 256c0 105.9-86.13 192-192 192c-50.06 0-95.56-19.42-129.8-50.91l270.9-270.9C428.6 160.4 448 205.9 448 256z"/></svg>
+                        <h3 class="mt-2 text-xl font-medium text-gray-900">🎉 Super !</h3>
+                        <p class="mt-1 text-sm text-gray-500">
+                            il n'y a aucune tâche sur le dossier.
+                        </p>
+                    </div>
+                @endforelse
             </div>
             <div class="p-5 flex flex-col sm:flex-row items-center text-center sm:text-left text-slate-500">
                 <div></div>
