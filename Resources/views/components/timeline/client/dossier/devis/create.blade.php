3fdea494376047122cefa2b5de262ce91c57e4e8
gtko <gtko@users.noreply.github.com>
2022-05-10 16:08:59 +0200

    fix timeline

--- Resources/views/components/timeline/client/dossier/devis/create.blade.php
+++ Resources/views/components/timeline/client/dossier/devis/create.blade.php
@@ -3,10 +3,9 @@
         <img alt="" src="{{$flow->datas->getUser()->avatar_url}}"/>
     </x-slot>
     <div class="flex items-center">
+        @if($flow->datas->getDevis())
         <div class="font-medium">
             Création du
-
-
             <x-corecrm::timeline.timeline-item-link :url="route('devis.edit', [$flow->datas->getDevis()->dossier->client, $flow->datas->getDevis()->dossier, $flow->datas->getDevis()])">
                 devis#{{$flow->datas->getDevis()->ref}}
             </x-corecrm::timeline.timeline-item-link>
@@ -16,6 +15,7 @@
                 {{$flow->datas->getUser()->format_name}}
             </x-corecrm::timeline.timeline-item-link>
         </div>
+        @endif
         <div class="text-xs text-gray-500 ml-auto">{{$flow->created_at->format('H:i')}}</div>
     </div>
 </x-corecrm::timeline-item>
