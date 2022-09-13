25dff494231bf22978638d166293dfafbc94db29
gtko <gtux.prog@gmail.com>
2022-09-13 07:35:01 +0200

    add new btn add workflow

--- Resources/views/app/workflows/index.blade.php
+++ Resources/views/app/workflows/index.blade.php
@@ -3,6 +3,11 @@
         <x-basecore::breadcrumb-item>Workflow</x-basecore::breadcrumb-item>
     </x-slot>
     <x-basecore::layout.panel-full>
+        <div class="flex flex-wrap justify-between sm:flex-nowrap items-center mt-2">
+            <a href="{{route('workflows.create')}}" class="btn btn-primary shadow-md mr-2">
+                @icon('addCircle', null, 'mr-2') Nouveau workflow
+            </a>
+        </div>
         <livewire:corecrm::workflow-datatable />
     </x-basecore::layout.panel-full>
 </x-basecore::app-layout>
